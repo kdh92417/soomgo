cee351379e06549f875c8e1053445443ce77d2a2
jayden <guitarlove00@gmail.com>
2020-07-16 19:40:46 +0900

    [Fix] Modify conflicts

--- soomgo/settings.py
+++ soomgo/settings.py
@@ -36,13 +36,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-<<<<<<< HEAD
     'corsheaders',
     'account',
     'category'
-=======
-    'corsheaders'
->>>>>>> dbac424... [Fix] migrate models
 ]
 
 MIDDLEWARE = [
--- soomgo/urls.py
+++ soomgo/urls.py
@@ -1,9 +1,5 @@
-<<<<<<< HEAD
 from django.urls import path, include
 
 urlpatterns = [
    path('category', include('category.urls')),
 ]
-=======
-from django.urls import path
->>>>>>> dbac424... [Fix] migrate models
